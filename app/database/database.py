7c6228f64dd4d8a155273fcba1b3b23bd79c65b5
Jesna Jose <jesnaa384@gmail.com>
2026-07-03 02:38:23 +0530

    Fix DATABASE_URL

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-DATABASE_URL = os.getenv("postgresql://order_management_fcyj_user:ZLswT9ciUiinx4AUr9tNAfv0UqtjmKFN...")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
