9df0c2319dace3fece2a3565a2205e7834faaa81
Jesna Jose <jesnaa384@gmail.com>
2026-07-03 03:53:09 +0530

    Fix CORS

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Order Management API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
